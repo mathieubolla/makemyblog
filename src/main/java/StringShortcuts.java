1d1b53b2fca90e5b496a52cfa202d7e76e9055e3
Mathieu Bolla <mathieubolla@MacBook-Pro-de-Helene.local>
2013-11-03 19:53:51 +0100

    Remove bad extension for texts

--- src/main/java/StringShortcuts.java
+++ src/main/java/StringShortcuts.java
@@ -1,6 +1,9 @@
 public class StringShortcuts {
     public static String makeTitleFrom(String baseName) {
-        return baseName.substring(baseName.lastIndexOf('/') + 1, baseName.length()).replace('_', ' ');
+        String withExtension = baseName.substring(baseName.lastIndexOf('/') + 1, baseName.length()).replace('_', ' ');
+        if (withExtension.contains(".")) {
+            return withExtension.substring(0, withExtension.lastIndexOf('.'));
+        }
+        return withExtension;
     }
-
 }
